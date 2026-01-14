7fc52990add787c5d92720287ccad17913183a0c
bodhiintellect <nath.syam.1986@gmail.com>
2026-01-14 09:58:34 +0530

    feat: Add member type filtering to Return Book quick select

--- circulation/views.py
+++ circulation/views.py
@@ -160,7 +160,7 @@ class ReturnBookView(LoginRequiredMixin, LibrarianRequiredMixin, View):
         context = {
             'returned_today_count': BorrowRecord.objects.filter(return_date__date=today).count(),
             'recent_returns': BorrowRecord.objects.filter(status='RETURNED').select_related('user', 'book').order_by('-return_date')[:5],
-            'members': User.objects.filter(role='MEMBER').values('username', 'first_name', 'last_name', 'email')
+            'members': User.objects.all().values('username', 'first_name', 'last_name', 'email', 'role', 'membership_tier__name')
         }
         return context
 
